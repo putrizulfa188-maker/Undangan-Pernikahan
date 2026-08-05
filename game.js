97bda9e346ae45a82431c8ed32ea49aa75927d1f
putrizulfa188-maker <putrizulfa188@gmail.com>
2026-08-05 10:02:45 +0700

    Update game.js

--- game.js
+++ game.js
@@ -14,10 +14,8 @@ const config = {
     backgroundColor: "#000000",
 
     scale: {
-        mode: Phaser.Scale.EXPAND,
-        autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1280,
-        height: 720
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
 
     physics: {
